a08e910ed0337d5e0f81e79355ebb58ec76307ac
Kobe Lin <kobelin@linzijiede-MacBook-Pro.local>
2017-06-26 22:56:08 +0800

    hw4 hello

---
+++ tests/helloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+        std::cout << "103703040\n";
+
+    return 0;
+}
---
+++ tests/unistd/close.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <assert.h>
+
+int main() {
+  int f = open("/", O_RDONLY);
+
+  int ret = fsync(f);
+  printf("fsync(opened): %d\n", ret);
+  printf("errno: %d\n", errno);
+  assert(ret == 0);
+  assert(errno == 0);
+  errno = 0;
+
+  ret = close(f);
+  printf("close(opened): %d\n", ret);
+  printf("errno: %d\n", errno);
+  assert(ret == 0);
+  assert(errno == 0);
+  errno = 0;
+
+  ret = fsync(f);
+  printf("fsync(closed): %d\n", ret);
+  printf("errno: %d\n", errno);
+  assert(ret == -1);
+  assert(errno == EBADF);
+  errno = 0;
+
+  ret = close(f);
+  printf("close(closed): %d\n", ret);
+  printf("errno: %d\n", errno);
+  assert(ret == -1);
+  assert(errno == EBADF);
+  errno = 0;
+
+#ifdef REPORT_RESULT
+  int result = 0;
+  REPORT_RESULT();
+#endif
+  return 0;
+}
